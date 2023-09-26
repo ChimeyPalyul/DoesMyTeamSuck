97f43240def0f672019ea43a8866bc16d3964ec9
Chimey Palyul <palyul.chimey@gmail.com>
2023-09-26 16:51:05 -0600

    x button

--- index.js
+++ index.js
@@ -86,6 +86,7 @@ function displayTeam(data) {
 function additionalDetails(team){
     let currentTeam = team;
     let aDetails = document.querySelector('#additionalInfo')
+    let li = document.createElement('li')
     let title = document.createElement('h2')
     title.textContent = "Additional Details"
     let namE = document.createElement('p')
@@ -95,6 +96,8 @@ function additionalDetails(team){
     let xGfor = document.createElement('p')
     let xGagainst = document.createElement('p')
     let dmtsBtn = document.createElement('btn')
+    let dltBtn = document.createElement('btn')
+
 
     namE.textContent = `${currentTeam.teamName}`
     points.textContent = `Points: ${currentTeam.points}`
@@ -104,10 +107,26 @@ function additionalDetails(team){
     xGagainst.textContent = `Expected Goals Conceded: ${currentTeam.xGAgainst}`
     dmtsBtn.className = 'Button'
     dmtsBtn.textContent = "Does My Team Suck?!?"
+    dltBtn.textContent = "x"
+    dltBtn.className = "dltBtn"
+
+
+    li.append(namE,points,goalsFour,goalsAgainst,xGfor,xGagainst,dmtsBtn,dltBtn)
+
+    dltBtn.addEventListener("click", (e) =>{
+        aDetails.innerHTML = ""
+        
+    })
+
+
+
     dmtsBtn.addEventListener('click', (e) => {
         const qualityQuotient = (team.xGD/team.matches)*100
-       if ( qualityQuotient > 100) {
-        return alert("Your team is good. Very good.")
+        if ( qualityQuotient > 100 && currentTeam.teamName === "Manchester City") {
+            return alert("Your team is good.Very good.")
+            }
+        else if ( qualityQuotient > 100) {
+        return alert("Watch out Pep! This team is good. Very good.")
         } else if (qualityQuotient <= 100 && qualityQuotient > 50) {
             return alert('Keep this up and you\'ll be playing in Europe next season!')
         } else if (qualityQuotient <= 50 && qualityQuotient > 0) {
@@ -115,8 +134,10 @@ function additionalDetails(team){
         } else if (qualityQuotient <= 0 && qualityQuotient > -50) {
             return alert ('You probablyyyyy won\'t be relegated?')
         } return alert('Have fun in the Championship next year.')
+
+    
 })
-    aDetails.append(namE,points,goalsFour,goalsAgainst,xGfor,xGagainst,dmtsBtn)
+    aDetails.append(li)
 }
 
 
